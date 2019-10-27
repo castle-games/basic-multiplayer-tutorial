fd247da01479c5331f2d654199dc4a2c509b274e
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-10-27 02:31:58 -0700

    initial position

--- client.lua
+++ client.lua
@@ -5,6 +5,9 @@ local client = clientServer.client
 client.enabled = true
 client.start('127.0.0.1:22122')
 
+local share = client.share
+local home = client.home
+
 function client.connect()
     print('client: connected to server')
 end
@@ -19,6 +22,16 @@ function client.receive(message, ...)
         if otherClientId ~= client.id then
             print('client: other client ' .. otherClientId .. " pressed key '" .. key .. "'")
         end
+    end
+end
+
+function client.draw()
+    if client.connected then
+        for clientId, player in pairs(share.players) do
+            love.graphics.rectangle('fill', player.x - 20, player.y - 20, 40, 40)
+        end
+    else
+        love.graphics.print('connecting...', 20, 20)
     end
 end
 
--- server.lua
+++ server.lua
@@ -5,8 +5,20 @@ local server = clientServer.server
 server.enabled = true
 server.start('22122')
 
+local share = server.share
+local homes = server.homes
+
+function server.load()
+    share.players = {}
+end
+
 function server.connect(clientId)
     print('server: client ' .. clientId .. ' connected')
+
+    share.players[clientId] = {
+        x = math.random(40, 800 - 40),
+        y = math.random(40, 450 - 40),
+    }
 end
 
 function server.disconnect(clientId)
