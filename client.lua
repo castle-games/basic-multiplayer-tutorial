949977a0172f2e2e4229cce5d16f8f76f62e4438
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-10-27 00:12:54 -0700

    testing the local connection

--- client.lua
+++ client.lua
@@ -4,3 +4,11 @@ local client = clientServer.client
 
 client.enabled = true
 client.start('127.0.0.1:22122')
+
+function client.connect()
+    print('client: connected to server')
+end
+
+function client.disconnect()
+    print('client: disconnected from server')
+end
--- server.lua
+++ server.lua
@@ -4,3 +4,11 @@ local server = clientServer.server
 
 server.enabled = true
 server.start('22122')
+
+function server.connect(clientId)
+    print('server: client ' .. clientId .. ' connected')
+end
+
+function server.disconnect(clientId)
+    print('server: client ' .. clientId .. ' disconnected')
+end
